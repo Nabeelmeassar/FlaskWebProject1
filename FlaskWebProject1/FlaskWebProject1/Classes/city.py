2bf60fc6873e60e94a14a4138c62a06a82d580e6
nn <nn@DESKTOP-GPIVTI7>
2024-01-20 13:42:04 +0100

    freie tage anpassen

--- FlaskWebProject1/FlaskWebProject1/Classes/city.py
+++ FlaskWebProject1/FlaskWebProject1/Classes/city.py
@@ -4,7 +4,7 @@ from geopy.distance import geodesic
 # Eine Klasse, die die Informationen über eine Stadt speichert
 class City:
     cities = {}
-    def __init__(self,id, name, gps, rating, hotel_cost, ticket_cost, driving_cost, distance_km):
+    def __init__(self,id, name, gps, rating, hotel_cost, ticket_cost, driving_cost, distance_km, days_of_stay):
         self.name = name
         self.id = id
         self.gps = gps # Ein Tupel aus (latitude, longitude)
@@ -13,6 +13,7 @@ class City:
         self.ticket_cost = float(ticket_cost)
         self.driving_cost = float(driving_cost)
         self.distance_km = float(distance_km)
+        self.days_of_stay = int(days_of_stay)
     def to_dict(self):
         # Convert the City object to a dictionary
         return {
@@ -23,7 +24,8 @@ class City:
             'hotel_cost': self.hotel_cost,
             'ticket_cost': self.ticket_cost,
             'driving_cost': self.driving_cost,
-            'distance_km': self.distance_km
+            'distance_km': self.distance_km,
+            'days_of_stay': self.days_of_stay
         }
         
     def update_hotel_cost(self, new_hotel_cost):
@@ -33,7 +35,10 @@ class City:
         self.ticket_cost = float(new_ticket_cost)
 
     def update_rating(self, new_rating):
-        self.rating = new_rating
+        self.rating = new_rating  
+        
+    def update_days_of_stay(self, new_days_of_stay):
+        self.days_of_stay = new_days_of_stay
         
     def get_cost(self):
         cost = self.hotel_cost + self.ticket_cost + self.driving_cost
@@ -55,8 +60,7 @@ def get_cities():
         # Schritt 3: Erstellen von City-Objekten und Hinzufügen zur Liste
         for row in reader:
             # Angenommen, jede Zeile hat Stadtname, Bevölkerung und Land in dieser Reihenfolge
-            city = City(row[0], row[1],(row[4],row[5]),0 , row[6], row[7], 0.0, 0.0)
+            city = City(row[0], row[1],(row[4],row[5]),0 , row[6], row[7], 0.0, 0.0, 0)
             cities[city.name] = city
     return cities
-
 
